11c027f0ce8b9fb4be45850879bdc9926480e3c3
Rohan-here <guptarohan2002@gmail.com>
2021-10-20 12:38:44 +0530

    added backend structure

--- Web Dev Projects/ultimate scraper/backend/app.js
+++ Web Dev Projects/ultimate scraper/backend/app.js
@@ -9,12 +9,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    console.log(req.body);
-    res.status(200).json({ data: req.body });
+
+
+app.use("/", require("./routes"));
+
+//Default Route
+app.get("/", (req, res) => {
+    return res.status(200).json({ working: true });
 });
 
+app.get('/*', (req, res) => {
+    return res.status(404).json({ message: "Route does not exist :(" })
+})
 
 const port = process.env.port;
 app.listen(port, () => { console.log(`Listening on ${port}`) });
---
+++ Web Dev Projects/ultimate scraper/backend/routes/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.use('/api', require("./api"))
+
+
+
+module.exports = router;
