4c9c9a7cb8bb43facfaea8354d823dc571fb9448
soumyo2002 <92665254+soumyo2002@users.noreply.github.com>
2021-10-22 18:57:56 +0530

    Create rotate_array.java

---
+++ rotate_array.java
@@ -0,0 +1,45 @@
+class RotateArray 
+{
+    void leftRotate(int arr[], int d, int n) 
+    {
+        int i, j, k, temp;
+        for (i = 0; i < gcd(d, n); i++) 
+        {
+            /* move i-th values of blocks */
+            temp = arr[i];
+            j = i;
+            while (true) 
+            {
+                k = j + d;
+                if (k >= n) 
+                    k = k - n;
+                if (k == i) 
+                    break;
+                arr[j] = arr[k];
+                j = k;
+            }
+            arr[j] = temp;
+        }
+    }
+    void printArray(int arr[], int size) 
+    {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+    }
+  
+    /*Function to get gcd of a and b*/
+    int gcd(int a, int b) 
+    {
+        if (b == 0)
+            return a;
+        else
+            return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        RotateArray rotate = new RotateArray();
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        rotate.leftRotate(arr, 2, 7);
+        rotate.printArray(arr, 7);
+    }
+}
