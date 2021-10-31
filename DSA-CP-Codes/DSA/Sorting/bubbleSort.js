ad8d3f362d4b5cdaa50fc509b7b17e62f9c851c9
SanyamJain <sanyamjain10168@gmail.com>
2021-10-31 15:20:58 +0530

    bubbleSort

---
+++ DSA-CP-Codes/DSA/Sorting/bubbleSort.js
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+        {
+            return;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {6, 5, 4, 3, 2, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    bubble_sort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
